c6b984df3f198bcdfe9b904dd0c02fe2bffd62e2
Andrés G. de la Filia <57175754+agdelafilia@users.noreply.github.com>
2020-09-24 16:38:52 +0100

    Update B_cov_analysis_no_mismatches.R

--- R_scripts/B_cov_analysis_no_mismatches.R
+++ R_scripts/B_cov_analysis_no_mismatches.R
@@ -212,3 +212,116 @@ table(genes.in.B.strict.transcriptome.expr$info)
 # export lists
 # write.csv(B.strict,"/Users/agarcia/Documents/genomics/B_viburni_ross_lab/data/coverage_analysis/B.strict.primary.reads.mapped.no.mismatches.csv", row.names = FALSE)
 # write.csv(genes.in.B.strict.anno,"/Users/agarcia/Documents/genomics/B_viburni_ross_lab/data/coverage_analysis/genes.in.B.strict.anno.csv", row.names = FALSE)
+
+# get alignments of SPAdes assemblies to the Pacbio reference
+
+# import files: many-to-many
+pviburni.freeze <- read_delim("p.viburni.freeze.v0.softmasked.fa.fai", 
+                              "\t", escape_double = FALSE, col_names = FALSE, 
+                              trim_ws = TRUE)
+B_strict.candidates <- read_csv("B.strict.candidates", 
+                                col_names = FALSE)
+B_strict.candidates$B.candidate <- "Y"
+
+colnames(pviburni.freeze)[1] <- "scaffold"
+colnames(pviburni.freeze)[2] <- "len"
+
+B_strict.candidates$B.candidate <- "Y"
+colnames(B_strict.candidates)[1] <- "scaffold"
+
+spades.nucmer.04 <- read_csv("spades/04.spades.v.freeze.v0.dnadiff.mcoords.list", col_names = FALSE)
+spades.nucmer.13 <- read_csv("spades/13.spades.v.freeze.v0.dnadiff.mcoords.list", col_names = FALSE)
+spades.nucmer.21 <- read_csv("spades/21.spades.v.freeze.v0.dnadiff.mcoords.list", col_names = FALSE)
+spades.nucmer.23 <- read_csv("spades/23.spades.v.freeze.v0.dnadiff.mcoords.list", col_names = FALSE)
+
+# create
+
+pviburni.scaffold.table <- left_join(pviburni.freeze[c(1,2)],B_strict.candidates,by="scaffold")
+
+colnames(spades.nucmer.04)[1] <- "scaffold"
+colnames(spades.nucmer.13)[1] <- "scaffold"
+colnames(spades.nucmer.21)[1] <- "scaffold"
+colnames(spades.nucmer.23)[1] <- "scaffold"
+spades.nucmer.04$PV.04 <- "Y"
+spades.nucmer.13$PV.13 <- "Y"
+spades.nucmer.21$PV.21 <- "Y"
+spades.nucmer.23$PV.23 <- "Y"
+
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.04,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.13,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.21,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.23,by="scaffold")
+
+# how many scaffolds are in the B+ lines and not in the B- lines?
+
+pviburni.scaffold.in.Bplus.m <- pviburni.scaffold.table[!is.na(pviburni.scaffold.table$PV.13) & !is.na(pviburni.scaffold.table$PV.04)
+                                                        & is.na(pviburni.scaffold.table$PV.21) & is.na(pviburni.scaffold.table$PV.23)
+                                                        & !is.na(pviburni.scaffold.table$B.candidate),]
+
+count(pviburni.scaffold.in.Bplus.m$B.candidate)
+sum(pviburni.scaffold.in.Bplus.m$len)
+
+# reimport files: 1-to-1
+
+spades.nucmer.04 <- read_csv("spades/04.spades.v.freeze.v0.dnadiff.1coords.list", col_names = FALSE)
+spades.nucmer.13 <- read_csv("spades/13.spades.v.freeze.v0.dnadiff.1coords.list", col_names = FALSE)
+spades.nucmer.21 <- read_csv("spades/21.spades.v.freeze.v0.dnadiff.1coords.list", col_names = FALSE)
+spades.nucmer.23 <- read_csv("spades/23.spades.v.freeze.v0.dnadiff.1coords.list", col_names = FALSE)
+
+# create
+
+pviburni.scaffold.table <- left_join(pviburni.freeze[c(1,2)],B_strict.candidates,by="scaffold")
+
+colnames(spades.nucmer.04)[1] <- "scaffold"
+colnames(spades.nucmer.13)[1] <- "scaffold"
+colnames(spades.nucmer.21)[1] <- "scaffold"
+colnames(spades.nucmer.23)[1] <- "scaffold"
+spades.nucmer.04$PV.04 <- "Y"
+spades.nucmer.13$PV.13 <- "Y"
+spades.nucmer.21$PV.21 <- "Y"
+spades.nucmer.23$PV.23 <- "Y"
+
+nrow(spades.nucmer.04)
+nrow(spades.nucmer.13)
+nrow(spades.nucmer.21)
+nrow(spades.nucmer.23)
+
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.04,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.13,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.21,by="scaffold")
+pviburni.scaffold.table <- left_join(pviburni.scaffold.table,spades.nucmer.23,by="scaffold")
+
+# how many scaffolds are in the B+ lines and not in the B- lines?
+
+pviburni.B.scaffolds <- pviburni.scaffold.table[!is.na(pviburni.scaffold.table$PV.13) & !is.na(pviburni.scaffold.table$PV.04)
+                                                & is.na(pviburni.scaffold.table$PV.21) & is.na(pviburni.scaffold.table$PV.23),]
+count(pviburni.B.scaffolds$B.candidate)
+
+# let's plot this
+
+reads.B.lines.spades <- reads.B.lines
+reads.B.lines.spades$b.status <- ifelse(reads.B.lines.spades$seq %in% pviburni.B.scaffolds$scaffold & reads.B.lines.spades$b.status == "B.strict", "B.strict.plus.assembly", reads.B.lines.spades$b.status)
+reads.B.lines.spades$b.status <- ifelse(reads.B.lines.spades$seq %in% pviburni.B.scaffolds$scaffold & reads.B.lines.spades$b.status == "B.loose", "B.loose.plus.assembly", reads.B.lines.spades$b.status)
+reads.B.lines.spades$b.status <- ifelse(reads.B.lines.spades$seq %in% pviburni.B.scaffolds$scaffold & reads.B.lines.spades$b.status == "A", "B.assembly", reads.B.lines.spades$b.status)
+count(reads.B.lines.spades$b.status)
+
+reads.B.lines.spades$b.status <- factor(reads.B.lines.spades$b.status, levels = c("B.strict.plus.assembly","B.strict","B.loose.plus.assembly","B.loose","B.assembly","A"))
+
+
+p1 <- ggplot(reads.B.lines.spades, aes(log10(PV13.read.cov+1e-4),log10(PV04.read.cov+1e-4))) + geom_point(aes(colour=b.status),size=1) +
+  scale_color_manual(values=c("royalblue4", "dodgerblue", "green4", "green1", "lavenderblush4", "lavenderblush1")) +
+  labs(title="log10(norm read cov + 1e-4)", y="PV04", x = "PV13") + theme_bw()
+
+reads.B.lines.cov <- reads.B.lines[c(1,5,6,7)]
+colnames(reads.B.lines.cov)[3] <- "PV13"
+colnames(reads.B.lines.cov)[4] <- "PV04"
+
+reads.B.lines.long <- melt(reads.B.lines.cov, id.vars=c("seq","b.status"))
+colnames(reads.B.lines.long)[3] <- "B.line"
+colnames(reads.B.lines.long)[4] <- "read.cov"
+
+p2 <- ggplot(reads.B.lines.long, aes(B.line, log10(read.cov+1e-4),fill=b.status)) +
+  geom_boxplot(alpha=0.75,outlier.shape = NA,notch=TRUE,lwd=0.6) + #ylim(-1.5,2) +
+  scale_fill_manual(breaks = c("A","B.loose","B.strict"), values = c("azure3", "darkgreen", "deeppink3")) + 
+  theme_bw() 
+
