31c84fccde912ee62ba1e124192bf2cd51abba27
Andrés G. de la Filia <57175754+agdelafilia@users.noreply.github.com>
2020-11-05 17:32:11 +0000

    Create EdgeR-PviburniB_GENE_andres.R

---
+++ R_scripts/EdgeR-PviburniB_GENE_andres.R
@@ -0,0 +1,289 @@
+rm(list=ls())
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("edgeR")
+rm(list=ls())
+library(edgeR)
+library(methods)
+library(dplyr)
+library(tidyverse)
+library(limma)
+#BiocManager::install("Glimma")
+library(Glimma)
+library(gplots)
+#BiocManager::install("org.Mm.eg.db")
+library(org.Mm.eg.db)
+
+# get annotation file
+
+setwd("/Users/agarcia/Documents/genomics/B_viburni_ross_lab/data")
+
+genetotranscript<-read.delim("annotation/p.viburni.freeze.v0.braker.transcripts.to.genes.txt", header=FALSE)
+genetotranscript <-genetotranscript[order(genetotranscript$V1),]
+
+interpro<-read.delim("annotation/p.viburni.freeze.v0.braker.interproscan.tsv", header = FALSE) #GO terms with transcript id but no gene id
+interpro <-interpro[order(interpro$V1),]
+
+sampleinfo<-read.csv("/Users/agarcia/Documents/genomics/B_viburni_ross_lab/R_scripts/sampleinfoPviburniB.csv") #sample group info
+
+head(genetotranscript,2) #col1 is gene id, col 2 is transcript id
+names(genetotranscript)<-c("gid","tid") #gid=gene id, tig = transcript id
+
+head(interpro,2) #column V1 is transcript id
+names(interpro)[names(interpro) == 'V1'] <- 'tid'
+head(interpro,2)
+
+annot = merge(genetotranscript,interpro,by.x="tid", by.y="tid",all=TRUE)
+head(annot)
+
+# count file from all samples
+# need a dataframe containing all gene info per gene id
+rsem.counts <-read.delim("/Users/agarcia/Documents/genomics/B_viburni_ross_lab/R_scripts/RSEM_digi.counts.matrix",header=TRUE) #matrix generated from rsem
+a <- rsem.counts
+colnames(rsem.counts) <- 
+head(a,2) 
+
+colnames(a) <- substr(colnames(a),start=1,stop=6) #removing ".genes.results" in colnames
+colnames(a)
+
+#first column X is the gene id, this needs to be removed but has the order of gene id has to match when merging with gene info
+head(a) 
+a1 <- a[,-1]
+head(a1)
+rownames(a1) <- a[,1]
+head(a1)
+a<-a1
+head(a)
+
+#adding replicate information
+replicate = as.factor(c("X04F","X04F","X04F","X04M","X04M","X04M","X13F","X13F","X13F","X13M","X13M","X13M","X13M","X15F","X15F","X15F","X15M","X15M","X15M","X21F","X21F","X21F","X21M","X21M","X21M","X21M"))
+
+#create the DGE object including all count matrix, replicate info and annotations when available
+x<-DGEList(counts=round(a),genes=rownames(a), group = replicate)
+head(x)
+
+# filter out low count
+
+rowSums(x$counts==0) # for each sample where the count is 0, then sums the samples that have counts = 0 for a gene
+table(rowSums(x$counts==0) == 26) #number of genes that have all samples with 0 counts
+proportion_0_count= 21236/2393
+proportion_0_count
+
+dim(x)
+
+# compare filtering options. By default, the function keeps genes with about 10 read counts or more in a minimum number of samples, where the number of samples is chosen according to the minimum group sample size. The actual filtering uses CPM values rather than counts in order to avoid giving preference to samples with large library sizes. For this dataset, the median library size is about 51 million and 10/51 approx. 0.2, so the filterByExpr
+keep.exprs.group <- filterByExpr(x, group=x$samples$group,min.count=5, min.prop = 20) 
+keep.exprs.sample <- filterByExpr(x, group=rownames(x$samples),min.count=5, min.prop = 20) # keep genes with min count of 5 in at least 20% of samples
+
+x1 <- x[keep.exprs.group, keep.lib.sizes=FALSE]
+x2 <- x[keep.exprs.sample, keep.lib.sizes=FALSE]
+
+dim(x)
+dim(x1)
+dim(x2) 
+
+#using filtering by sample
+x<-x2
+
+# plot  individual samples pre normalization
+for (i in 1:ncol(x)) {
+	plotMD(cpm(x, log=TRUE), column=i, xlab = "Average log-expression", ylab = "Expression log-ratio (this sample vs others)", main = colnames(x)[i])
+	abline(h=0, col="red", lty=2, lwd=2)
+}
+
+# normalize distribution (TMM normalization)
+x <- calcNormFactors(x, method = "TMM")
+x$samples$norm.factors
+
+# plot  individual samples post normalization
+for (i in 1:ncol(x)) {
+	plotMD(cpm(x, log=TRUE), column=i, xlab = "Average log-expression", ylab = "Expression log-ratio (this sample vs others)", main = colnames(x)[i])
+	abline(h=0, col="red", lty=2, lwd=2)
+}
+
+# QC
+x$samples$lib.size
+barplot(x$samples$lib.size,names=colnames(x),las=2)
+title("Barplot of library sizes")
+logcounts <- cpm(x,log=TRUE)
+boxplot(logcounts, xlab="", ylab="Log2 counts per million",las=2)
+abline(h=median(logcounts),col="blue")
+title("Boxplots of logCPMs (normalized)")
+
+# MDS plot to check for grouping
+# checking grouping by sex, and by B presence
+
+sampleinfo2 <- sampleinfo %>% mutate(Sex = as.factor(Sex))
+levels(sampleinfo2$Sex)
+col.sex <- c("red","blue")[sampleinfo2$Sex]
+data.frame(sampleinfo2$Sex,col.sex)
+
+# plot by sex
+plotMDS(x,col=col.sex, main="Sex")
+legend("top",fill=c("red","blue"),legend=levels(sampleinfo2$Sex))
+
+# plot by B presence
+sampleinfo2 <- sampleinfo %>% mutate(Bpresence = as.factor(Bpresence))
+levels(sampleinfo2$Bpresence)
+col.B <- c("grey","red")[sampleinfo2$Bpresence]
+data.frame(sampleinfo2$Bpresence,col.B)
+plotMDS(x,col=col.B, main="B presence")
+legend("top",fill=c("grey","red"),legend=levels(sampleinfo2$Bpresence))
+
+#plot by genotype
+sampleinfo2 <- sampleinfo %>% mutate(Geno = as.factor(Geno))
+levels(sampleinfo2$Geno)
+col.geno <- c("green","red","blue","black")[sampleinfo2$Geno]
+data.frame(sampleinfo2$Geno,col.geno)
+plotMDS(x,col=col.geno, main="Genotype")
+legend("top",fill=c("green","red","blue","black"),legend=levels(sampleinfo2$Geno))
+
+## Hierarchical clustering
+var_genes <- apply(logcounts, 1, var)
+head(var_genes)
+select_var <- names(sort(var_genes, decreasing=TRUE))[1:1000]
+head(select_var)
+highly_variable_lcpm <- logcounts[select_var,]
+dim(highly_variable_lcpm)
+head(highly_variable_lcpm)
+
+# plot the heatmap by sex
+library(RColorBrewer)
+mypalette <- brewer.pal(11,"RdYlBu")
+morecols <- colorRampPalette(mypalette)
+heatmap.2(highly_variable_lcpm,col=rev(morecols(50)),trace="none", main="Top 1000 most variable genes across samples",ColSideColors=col.sex,scale="row")
+
+# plot the heatmap by B presence
+heatmap.2(highly_variable_lcpm,col=rev(morecols(50)),trace="none", main="Top 1000 most variable genes across samples",ColSideColors=col.B,scale="row")
+
+## Model design
+
+# The model design and fit is based on sex by B presence groups. Model will be fit based on this group, then we can compare pairs of groups by creating different contrast matrices.
+
+# define groups by sex and B presence or absence
+group1=c("FB","FB","FB","MB","MB","MB","FB","FB","FB","MB","MB","MB","MB","FnoB","FnoB","FnoB","MnoB","MnoB","MnoB","FnoB","FnoB","FnoB","MnoB","MnoB","MnoB","MnoB")
+
+# design: Model Matrix by group defined above
+design1 <- model.matrix(~0 + group1)
+colnames(design1)
+rownames(design1) = rownames(x$samples)
+
+# removing heteroscedascity from count data: voom plots
+v1 <- voom(x,design1,plot = TRUE)
+v1
+
+# limma lm fit
+fit1 <- lmFit(v1)
+
+# comparison 1: all transcripts that are just B male
+# contrast matrix: called fit.cont1
+colnames(design1)
+
+# I only want transcripts differentially expressed in male B samples.
+cont.matrix1 <- makeContrasts(BmalevnoBmale = group1MB - group1MnoB, BmalevsfemaleB = group1MB - group1FB, BmalevsfemalenoB = group1MB - group1FnoB,BfemalevsnoBfemale = group1FB - group1FnoB, noBmalevsnoBfemale = group1MnoB - group1FnoB, levels=design1)
+cont.matrix1
+
+fit.cont1 <- contrasts.fit(fit1, cont.matrix1)
+fit.cont1 <- eBayes(fit.cont1)
+
+# compare mean-variance trend
+v1 <- voom(x,design1,plot = TRUE)
+plotSA(fit.cont1, main="Final model: Mean-variance trend")
+
+## Examine the number of DE genes
+
+tfit <- treat(fit.cont1, lfc=1) 
+dt <- decideTests(tfit)
+summary(dt)
+write.fit(tfit, dt, file="results.txt")
+
+# extracting only the B male specific
+
+# these are all the genes that are DE between B males and all the others
+de.B.vs.all <- which(dt[,1]!=0 & dt[,2]!=0 &dt[,3]!=0)
+length(de.B.vs.all)
+
+# these are all the genes that are over expressed and underexpressed, respectively, between B males and all the others
+de.over.B.males <- which(dt[,1]==1 & dt[,2]==1 &dt[,3]==1)
+de.under.B.males <- which(dt[,1]==-1 & dt[,2]==-1 &dt[,3]==-1)
+
+length(de.over.B.males) # B genes overexpressed in B males compared to the rest of the groups
+length(de.under.B.males) # B genes underexpressed in B males compared to the rest of the groups
+
+#Venn Diagram for B vs no B in males
+vennDiagram(dt[,1], circle.col=c("turquoise", "salmon","orange"),include=c("up","down"))
+
+#Venn Diagram for B vs no B in females
+vennDiagram(dt[,4], circle.col=c("turquoise", "salmon","orange"),include=c("up","down"))
+
+#Venn Diagram for Bmale
+vennDiagram(dt[,1:3], circle.col=c("turquoise", "salmon","orange"),include=c("up","down"))
+
+# list of genes from Bmale
+fit.cont1$genes[de.over.B.males,]
+fit.cont1$genes[de.under.B.males,]
+
+# export overexpressed genes in B males onlu
+de.over.B.males.genes <- data.frame(fit.cont1$genes[de.over.B.males,])
+colnames(de.over.B.males.genes) <- "gene"
+write.csv(de.over.B.males.genes,"output/diff_expr/over.Bmales.vs.all.csv")
+
+# export all lists of genes 
+
+de.Bm.vs.non.Bm <- which(dt[,1]!=0)
+length(de.Bm.vs.non.Bm)
+de.B.males.genes <- data.frame(fit.cont1$genes[de.Bm.vs.non.Bm,])
+nrow(de.B.males.genes)
+colnames(de.B.males.genes)[1] <- "id"
+
+maleB.noB.all <- data.frame(topTreat(fit.cont1, coef=1, n = Inf))
+maleB.noB.all$de <- ifelse(maleB.noB.all$genes %in% de.B.males.genes$id == TRUE,"DE",NA)
+View(maleB.noB.all)
+
+maleB.noB.all <- data.frame(topTreat(fit.cont1, coef=1, n = Inf))
+
+
+
+colnames(de.B.vs.all)[1] <- "genes"
+over <- left_join(de.B.vs.all, maleB.noB.all, by = c("genes"))
+
+
+nrow(maleB.noB_df)
+maleB.noB_df <- data.frame(maleB.noB) # 18749
+View(maleB.noB_df)
+nrow(maleB.noB_df[(maleB.noB_df$adj.P.Val < 0.05 & maleB.noB_df$logFC >= 1),])
+
+View(maleB.noB_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Make volcano plots
+
+pdf("rsem_gene_genomebased_MDplots.pdf", width=20, height=10, pointsize=12)
+par(mfrow=c(2,3))
+plotMD(fit.cont1, column=1, status=dt[,1], main=colnames(fit.cont1)[1], 
+       xlim=c(-8,13),cex=0.5)
+
+plotMD(fit.cont1, column=2, status=dt[,2], main=colnames(fit.cont1)[2], 
+       xlim=c(-8,13),cex=0.5)
+
+plotMD(fit.cont1, column=3, status=dt[,3], main=colnames(fit.cont1)[3], 
+       xlim=c(-8,13),cex=0.5)
+
+plotMD(fit.cont1, column=3, status=dt[,4], main=colnames(fit.cont1)[4], 
+       xlim=c(-8,13),cex=0.5)
+
+plotMD(fit.cont1, column=3, status=dt[,5], main=colnames(fit.cont1)[5], 
+       xlim=c(-8,13),cex=0.5)
+
